47a1cbfcb01cca81cde9ec80e47629e232642de9
Kyle Morris <43796793+Crypto84@users.noreply.github.com>
2024-04-30 10:43:17 +0000

    Reorganise Day 1 Structure

--- 1/Part2.js
+++ 1/Part2.js
@@ -3,12 +3,14 @@ const fs = require("fs")
 const inputString = fs.readFileSync("InputString.json", "utf8")
 const input = JSON.parse(inputString)
 let floors = 0
-input.parens.split("").forEach(element => {
+input.parens.split("").forEach((element, i) => {
     if(element === '(') {
         floors++
     } else {
         floors--
     }
-});
 
-console.log(floors)
+    if (floors === -1) {
+        console.log(i+1)
+    }
+});
