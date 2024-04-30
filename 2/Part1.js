c6bf75123e6c0ed5d11a3d2ebc13c9a261b21a34
Kyle Morris <43796793+Crypto84@users.noreply.github.com>
2024-04-30 10:43:42 +0000

    Fix day 2 part 2

--- 2/Part1.js
+++ 2/Part1.js
@@ -1,8 +1,6 @@
-// Not yet working!
-
 const fs = require("fs")
 
-function calculatePaper(l, w, h) {
+function calculatePaperRequired(l, w, h) {
     let minimumFace = Math.min(l*w, w*h, l*h)
     return (minimumFace + 2*l*w + 2*w*h + 2*l*h)
 }
@@ -11,7 +9,7 @@ const total = fs.readFileSync("./inputs.txt", "utf8")
     .split('\n')
     .reduce((total, line) => {
         measurements = line.split('x')
-        return(total + calculatePaper([...measurements]))
+        return(total + calculatePaperRequired(...measurements))
     }, 0)
 
 console.log(total)
